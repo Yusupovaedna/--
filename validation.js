008f272dae3e414f33eb3accaedf0f7b84a8c2e0
Nadya_k_ <nadya.karandasheva.2000@mail.ru>
2021-11-18 16:48:35 +0300

    Nadya

---
+++ validation.js
@@ -0,0 +1,24 @@
+function validation_form ( )
+{
+    let valid = true;
+
+    if ( document.forma.R.selectedIndex === 0 )
+    {
+        alert ( "Пожалуйста, выберите R." );
+        valid = false;
+    }
+
+    let y = this.y.value;
+    y = y.replace(/,/, ".");
+    const test = /[^\d-,.]/.test(y);
+
+    if (test || y ==="" || isNaN(y)) {
+        alert ( "Значение Y введено неверно. Y должен быть числом" );
+        valid = false;
+    }
+    else if (y<-5 || y>5){
+        alert ( "Y должен находиться в пределе от -5 до 5" );
+        valid = false;
+    }
+    return valid;
+}
