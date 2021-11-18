69f26055c42fb520815bb6f06f87d4031e61dfd2
Edna <yusupovaedna@gmail.com>
2021-11-19 01:37:44 +0300

    v-2

--- validation.js
+++ validation.js
@@ -1,25 +1,31 @@
+function validate() {
+    document.forma.elements['submit'].disabled = !(val());
+}
 
-function validation_form ( )
-{
-    let valid = true;
+function val() {
 
-    if ( document.forma.R.selectedIndex === 0 )
-    {
-        alert ( "Пожалуйста, выберите R." );
-        valid = false;
-    }
-
+    var valid = true;
     let y = this.y.value;
     y = y.replace(/,/, ".");
     const test = /[^\d-,.]/.test(y);
+    var txt = "";
 
-    if (test || y ==="" || isNaN(y)) {
-        alert ( "Значение Y введено неверно. Y должен быть числом" );
+    if (test || y === "" || isNaN(y)) {
+
+        txt = "Значение Y введено неверно. Y должен быть числом";
+        // alert ( "Значение Y введено неверно. Y должен быть числом" );
+        valid = false;
+
+    } else if (y < -5 || y > 5) {
+
+        txt = "Y должен находиться в пределе от -5 до 5";
+
+        // alert ( "Y должен находиться в пределе от -5 до 5" );
         valid = false;
     }
-    else if (y<-5 || y>5){
-        alert ( "Y должен находиться в пределе от -5 до 5" );
-        valid = false;
-    }
+    document.getElementById("demo").innerHTML = txt;
     return valid;
 }
+
+
+
